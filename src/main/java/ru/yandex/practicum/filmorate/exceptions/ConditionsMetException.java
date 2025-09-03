5745525b5e54f2dca4346448ac8b5a3853dec7d4
David_Yegorov <egor0vdavid@yandex.ru>
2025-09-03 14:40:04 +0300

    Initial commit

---
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ConditionsMetException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.exceptions;
+
+public class ConditionsMetException extends RuntimeException {
+    public ConditionsMetException(String message) {
+        super(message);
+    }
+}
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,6 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
@@ -10,17 +13,16 @@ import java.time.LocalDate;
 
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class Film {
-    @Null(groups = Marker.OnCreate.class)
+public class User {
     @NotNull(groups = Marker.OnUpdate.class)
     Long id;
-    @NotBlank(groups = Marker.OnCreate.class)
+    @Email
     @NotNull(groups = Marker.OnCreate.class)
+    String email;
+    @NotNull(groups = Marker.OnCreate.class)
+    @Pattern(regexp = "^[^\\s]+$")
+    String login;
     String name;
-    @Size(max = 200)
-    @NotNull(groups = Marker.OnCreate.class)
-    String description;
-    LocalDate releaseDate;
-    @Positive
-    Long duration;
+    @Past(groups = Marker.OnCreate.class)
+    LocalDate birthday;
 }
